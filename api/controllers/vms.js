d0ba42a8a5f5a7139bfe830a5825ed2301f3abab
Maël Valais <mael.valais@gmail.com>
2018-11-01 20:09:36 +0100

    add 'vm'

---
+++ api/controllers/vms.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/*
+ Modules make it possible to import JavaScript files into your application.  Modules are imported
+ using 'require' statements that give you a reference to the module.
+
+  It is a good idea to list the modules that your application depends on in the package.json in the project root
+ */
+/*
+ Once you 'require' a module you can reference the things that it exports.  These are defined in module.exports.
+
+ For a controller in a127 (which this is) you should export the functions referenced in your Swagger document by name.
+
+ Either:
+  - The HTTP Verb of the corresponding operation (get, put, post, delete, etc)
+  - Or the operationId associated with the operation in your Swagger document
+
+  In the starter/skeleton project the 'get' operation on the '/hello' path has an operationId named 'hello'.  Here,
+  we specify that in the exports of this module that 'hello' maps to the function named 'hello'
+ */
+module.exports = {
+  // a127 controller
+  vm: vm_a127
+};
+
+const vm = (req, res) => {
+  var name = req.swagger.params.name.value || "stranger";
+  res.json({ ok: "" });
+};
